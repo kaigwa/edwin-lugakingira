012d351c4459b534a44db7e6344406ee22fa9da1
Reset <kaigwaedwin@gmail.com>
2023-04-22 10:50:09 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import BetterWorld  from './components/BetterWorld';
+import About from "./components/About";
+import {
+  Routes,
+  Route,
+} from "react-router-dom"
+import KaigwaFarm from './components/KaigwaFarm';
+import KaigwaCup from './components/KaigwaCup';
+import Bububu from './components/Bububu';
+import Ellipsisdope from './components/Ellipsisdope';
+import Movie from './components/Movie';
+import Dar from './components/Dar';
+import Guadalajara from './components/Guadalajara';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={  <Home />}/>
+        <Route exact path="/Projects" element={<Projects />}/> 
+        <Route exact path="/BetterWorld" element={<BetterWorld />}/>
+        <Route exact path="/About" element={<About />}/>
+        <Route exact path="/KaigwaFarm" element={<KaigwaFarm/>}/>
+        <Route exact path="/KaigwaCup" element={<KaigwaCup/>}/>
+        <Route exact path="/Movie" element={<Movie/>}/>
+        <Route exact path="/Bububu" element={<Bububu/>}/>
+        <Route exact path="/Ellipsisdope" element={<Ellipsisdope/>}/>
+        <Route exact path="/Dar" element={<Dar/>}/>
+        <Route exact path="/Guadalajara" element={<Guadalajara/>}/>
+      </Routes>
     </div>
   );
 }
-
 export default App;
